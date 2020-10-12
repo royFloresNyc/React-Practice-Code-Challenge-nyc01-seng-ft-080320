f503c874729b82765cf4d589ad20dc1865287430
royFloresNyc <royfloresnyc@gmail.com>
2020-10-12 17:01:29 -0400

    Add money to wallet

---
+++ sushi-saga-client/src/components/AddMoney.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const AddMoney = (props) => {
+    const localChangeHandler = (e) =>{
+        props.formChangeHandler(e.target.value)
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        props.addMoney()
+    }
+
+    return (
+        <form onSubmit={submitHandler}>
+            <label>Add Money to Wallet:</label> <br/>
+            <input type="number" 
+                placholder="0" 
+                value={props.moneyFormVal} 
+                onChange={localChangeHandler}>
+            </input>
+            <input type="submit"></input>
+        </form>
+    )
+}
+
+export default AddMoney
